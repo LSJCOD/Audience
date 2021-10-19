fdae7e2194d0c056d63f4dea08f148de4c230e59
LSJCOD <libardo.salamanca.mt@correo.usa.edu.co>
2021-10-19 16:26:11 -0500

    Cambio tido de dato Reservation starDate y Devolution Date de String a Datetime-local

--- src/main/java/com/example/auditorio/Reservation/Reservation.java
+++ src/main/java/com/example/auditorio/Reservation/Reservation.java
@@ -15,8 +15,8 @@ public class Reservation implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idReservation;
-    private String startDate;
-    private String devolutionDate;
+    private Date startDate;
+    private Date devolutionDate;
     private String status="created";
 
     @ManyToOne
@@ -30,6 +30,7 @@ public class Reservation implements Serializable {
     private Client client;
     private String score;
 
+
     public Integer getIdReservation() {
         return idReservation;
     }
@@ -38,19 +39,19 @@ public class Reservation implements Serializable {
         this.idReservation = idReservation;
     }
 
-    public String getStartDate() {
+    public Date getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
-    public String getDevolutionDate() {
+    public Date getDevolutionDate() {
         return devolutionDate;
     }
 
-    public void setDevolutionDate(String devolutionDate) {
+    public void setDevolutionDate(Date devolutionDate) {
         this.devolutionDate = devolutionDate;
     }
 
